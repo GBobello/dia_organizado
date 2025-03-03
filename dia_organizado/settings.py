da89e312ba84e0d3e5ef79b4aaaedd13d6c4ff9b
GBobello <bobello.gabriel@gmail.com>
2025-03-03 11:00:09 -0300

    nothing

--- dia_organizado/settings.py
+++ dia_organizado/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-2fow^idymks*8^6%n4adb+ist^^eak-)k5$6sbc0lxuildxdq^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*", ".vercel.app"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -121,8 +121,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", "static")
+MEDIA_URLS = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 
 # Default primary key field type
