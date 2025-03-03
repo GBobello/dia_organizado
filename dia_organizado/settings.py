8226072da3e6c68c7eb7c8857eb3ad93129c6a33
GBobello <bobello.gabriel@gmail.com>
2025-03-03 10:53:36 -0300

    dependencias da vercel

--- dia_organizado/settings.py
+++ dia_organizado/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-2fow^idymks*8^6%n4adb+ist^^eak-)k5$6sbc0lxuildxdq^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["*", ".vercel.app"]
 
 
 # Application definition
@@ -79,12 +79,12 @@ WSGI_APPLICATION = "dia_organizado.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
 
 
 # Password validation
@@ -121,10 +121,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", "static")
-MEDIA_URLS = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 
 # Default primary key field type
